c570ed956f635cf2c0ab40a2a69be8d2c080188d
Dev Gricco <dev@gricco.com.br>
2025-11-07 10:25:34 -0300

    Pequenos ajustes para a nova identidade

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:3000',
-  baseURL: 'https://api.gricco.com.br',
+  baseURL: 'http://localhost:3000',
+  //baseURL: 'https://api.gricco.com.br',
+  //baseURL: process.env.NODE_ENV=="production" ? process.env.REACT_APP_API_URL : 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json'
   },
